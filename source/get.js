05a0e3ec1bb3e7601b285470024fde18f66ceb3e
Elena <oshkina95@gmail.com>
2018-02-16 00:31:44 +0300

    changed style

--- source/get.js
+++ source/get.js
@@ -2,18 +2,24 @@
 
 
 function get(obj, path){
-    let arr=path.slice(1).split('.');
-    if(arr[0].length==0)
+    const arr = path.slice(1).split('.');
+    
+    if (arr[0].length == 0)
         return obj;
 
-    let tempObj=obj;
-    path.slice(1)
-    .split('.')
-    .forEach(function(subpath) {
-        if(tempObj!=undefined && tempObj.hasOwnProperty(subpath))
-            tempObj=tempObj[subpath];
-        else
-            tempObj=undefined;
+    let tempObj = obj;
+    
+    /*path
+    .slice(1)
+    .split('.')*/
+    arr.forEach( (subpath) => {
+        if(tempObj != undefined && tempObj.hasOwnProperty(subpath)){
+            tempObj = tempObj[subpath];
+        }
+        else{
+            tempObj = undefined;
+        }
     });
+
     return tempObj;
-}
+} 
